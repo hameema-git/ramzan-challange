3c4882e199bd6798e735e92d2a1c42c7ff64a5c7
Hameema <hameema@gmail.com>
2026-02-25 00:31:56 +0530

    Updated firebase to use env variables

--- firebase.js
+++ firebase.js
@@ -2,14 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAV2uSO-TfyZ3br99zkf_GdA-XfdZ47fXY",
-  authDomain: "ramzan-global-leaderboard.firebaseapp.com",
-  projectId: "ramzan-global-leaderboard",
-  storageBucket: "ramzan-global-leaderboard.firebasestorage.app",
-  messagingSenderId: "180228666222",
-  appId: "1:180228666222:web:201ccc82c560505c4a877f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
-
 // Prevent re-initialization in Next.js
 const app = !getApps().length
   ? initializeApp(firebaseConfig)
